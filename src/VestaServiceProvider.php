c39c0c83dfbffef2fef39cfb7b34e441a1ca892e
Daniel Jones <daniel@steadfastcollective.com>
2018-06-18 16:17:56 +0100

    Remove config generator

--- src/VestaServiceProvider.php
+++ src/VestaServiceProvider.php
@@ -12,17 +12,16 @@ class VestaServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/vesta.php' => config_path('vesta.php'),
-            ], 'config');
-
-            /*
-            $this->loadViewsFrom(__DIR__.'/../resources/views', 'vesta');
-
-            $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/vesta'),
-            ], 'views');
-            */
+            // $this->publishes([
+            //     __DIR__.'/../config/vesta.php' => config_path('vesta.php'),
+            // ], 'config');
+            //
+            //
+            // $this->loadViewsFrom(__DIR__.'/../resources/views', 'vesta');
+            //
+            // $this->publishes([
+            //     __DIR__.'/../resources/views' => base_path('resources/views/vendor/vesta'),
+            // ], 'views');
         }
     }
 
